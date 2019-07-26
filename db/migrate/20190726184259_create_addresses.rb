a920de60fde4fd37aa911627cd0fc0dc8d64ca23
Jake Miller <jake0miller@gmail.com>
2019-07-26 16:04:51 -0600

    Make address migration

---
+++ db/migrate/20190726184259_create_addresses.rb
@@ -0,0 +1,14 @@
+class CreateAddresses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :addresses do |t|
+      t.string :nickname
+      t.string :address
+      t.string :city
+      t.string :state
+      t.integer :zip
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
