8a65fc21db8323494d936bd091636e5552150175
Jake Miller <jake0miller@gmail.com>
2019-07-27 17:59:16 -0600

    Validate uniqueness of address nickname per user

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,7 +2,8 @@ class Address < ApplicationRecord
   belongs_to :user
   has_many :orders
 
-  validates_presence_of :address, :city, :state, :zip, :nickname
+  validates_presence_of :address, :city, :state, :zip
+  validates :nickname, presence: true, uniqueness: { scope: :user_id}
 
   def shipped_orders
     orders.where(status: 'shipped')
